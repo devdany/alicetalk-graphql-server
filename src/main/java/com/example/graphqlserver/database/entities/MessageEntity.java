ac1a0df9920720cc917e8b88c247ecabfa2818f2
devdany <dayong7387@gmail.com>
2023-06-22 00:21:45 +0900

    database entity & repository preset set

--- src/main/java/com/example/graphqlserver/database/entities/MessageEntity.java
+++ src/main/java/com/example/graphqlserver/database/entities/MessageEntity.java
@@ -9,11 +9,13 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "messages")
 @AllArgsConstructor
+@NoArgsConstructor
 @Setter
 @Getter
 public class MessageEntity {
--- src/main/java/com/example/graphqlserver/database/repositories/ChatRepository.java
+++ src/main/java/com/example/graphqlserver/database/repositories/ChatRepository.java
@@ -1,9 +1,14 @@
 package com.example.graphqlserver.database.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.graphqlserver.database.entities.ChatEntity;
+import java.util.List;
+
 
 public interface ChatRepository extends JpaRepository<ChatEntity, String> {
-  
+  @Query("SELECT c FROM ChatEntity c JOIN c.members m WHERE m.id = :memberId")
+   List<ChatEntity> findChatsWithMember(@Param("memberId") String memberId);
 }
