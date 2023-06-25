eb4a27ed99cbec25c5d0e91de0deb3075f083d82
devdany <dayong7387@gmail.com>
2023-06-25 11:45:30 +0900

    fix: message sort

--- src/main/java/com/example/graphqlserver/database/repositories/MessageRepository.java
+++ src/main/java/com/example/graphqlserver/database/repositories/MessageRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.graphqlserver.database.entities.MessageEntity;
 
 public interface MessageRepository extends JpaRepository<MessageEntity, String> {
-  List<MessageEntity> findByChatId(String chatId);
+  List<MessageEntity> findByChatIdOrderByCreatedAtDesc(String chatId);
 }
